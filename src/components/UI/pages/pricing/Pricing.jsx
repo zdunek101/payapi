eab9a968fffa27b531a8f4afa1c2206a1729b3c1
BartoszZdunczyk <zdunczyk.bart@gmail.com>
2022-08-19 23:33:15 +0200

    Create Pricing.jsx

---
+++ src/components/UI/pages/pricing/Pricing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Title from "../../atoms/Title";
+import Plan from "../../organisms/pricing/Plan";
+import FormDemo from "../../molecules/FormDemo";
+
+const Pricing = () => {
+  return (
+    <>
+      <Title className={"pricing"} text={"Pricing"} />
+      <div className="plan__container">
+        <Plan className={"pricing"} mod={"--package"} title={"Free Plan"} price={"$0.00"} checkAmount={3} />
+        <Plan className={"pricing"} mod={"--package"} title={"Basic Plan"} price={"$249.00"} checkAmount={5} />
+        <Plan className={"pricing"} mod={"--package"} title={"Premium Plan"} price={"$499.00"} checkAmount={7} />
+      </div>
+      <FormDemo className={"pricing"} />
+    </>
+  );
+};
+
+export default Pricing;
