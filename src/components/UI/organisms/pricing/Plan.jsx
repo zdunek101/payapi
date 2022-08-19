4b67102b1892d4d6b050e8dca52b4ec3ac414410
BartoszZdunczyk <zdunczyk.bart@gmail.com>
2022-08-19 23:32:45 +0200

    Create Plan.jsx

---
+++ src/components/UI/organisms/pricing/Plan.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Title from "../../atoms/Title";
+import Text from "../../atoms/Text";
+import PlanBenefits from "../../molecules/PlanBenefits";
+import Buttons from "../../atoms/Buttons";
+
+const Plan = ({ className, title, price, checkAmount, mod }) => {
+  return (
+    <>
+      <div className="plan__package">
+        <Title className={className} text={title} mod={mod} />
+        {/* <Text className={className} text={text} /> */}
+        <p className={`${className}__price`}>{price}</p>
+        <PlanBenefits checkAmount={checkAmount} />
+        <Buttons className={"plan"} text={"Request Access"} type={"button"} href={"/contact"} />
+      </div>
+    </>
+  );
+};
+
+export default Plan;
