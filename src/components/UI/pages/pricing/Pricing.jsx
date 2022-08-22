bf1843ae55dbc06e06af40aceeff16e284dce403
BartoszZdunczyk <zdunczyk.bart@gmail.com>
2022-08-22 23:25:06 +0200

    Update Pricing.jsx

--- src/components/UI/pages/pricing/Pricing.jsx
+++ src/components/UI/pages/pricing/Pricing.jsx
@@ -8,9 +8,30 @@ const Pricing = () => {
     <>
       <Title className={"pricing"} text={"Pricing"} />
       <div className="plan__container">
-        <Plan className={"pricing"} mod={"--package"} title={"Free Plan"} price={"$0.00"} checkAmount={3} />
-        <Plan className={"pricing"} mod={"--package"} title={"Basic Plan"} price={"$249.00"} checkAmount={5} />
-        <Plan className={"pricing"} mod={"--package"} title={"Premium Plan"} price={"$499.00"} checkAmount={7} />
+        <Plan
+          className={"pricing"}
+          mod={"--package"}
+          title={"Free Plan"}
+          price={"$0.00"}
+          checkAmount={3}
+          text={"Build and test using our core set of products with up to 100 API requests "}
+        />
+        <Plan
+          className={"pricing"}
+          mod={"--package"}
+          title={"Basic Plan"}
+          price={"$249.00"}
+          checkAmount={5}
+          text={"Launch your project with unlimited requests and no contractual minimums "}
+        />
+        <Plan
+          className={"pricing"}
+          mod={"--package"}
+          title={"Premium Plan"}
+          price={"$499.00"}
+          checkAmount={7}
+          text={"Get tailored solutions, volume pricing, and dedicated support for your team "}
+        />
       </div>
       <FormDemo className={"pricing"} bottom={true} />
     </>
