fa8248c14449e50c02ce9eb4ae404bbffbcd234c
BartoszZdunczyk <zdunczyk.bart@gmail.com>
2022-09-05 21:37:15 +0200

    Update ContactPartners.jsx

--- src/components/UI/organisms/contact/ContactPartners.jsx
+++ src/components/UI/organisms/contact/ContactPartners.jsx
@@ -2,35 +2,68 @@ import React from "react";
 import styled from "styled-components";
 import { variables } from "../../../../settings/mainstyles";
 
-import tesla from "../../../files/assets/shared/desktop/tesla.svg";
-import microsoft from "../../../files/assets/shared/desktop/microsoft.svg";
-import hp from "../../../files/assets/shared/desktop/hewlett-packard.svg";
-import oracle from "../../../files/assets/shared/desktop/oracle.svg";
-import google from "../../../files/assets/shared/desktop/google.svg";
-import nvidia from "../../../files/assets/shared/desktop/nvidia.svg";
+import tesla from "../../../files/assets/shared/desktop/tesla-black.svg";
+import microsoft from "../../../files/assets/shared/desktop/microsoft-black.svg";
+import hp from "../../../files/assets/shared/desktop/hewlett-packard-black.svg";
+import oracle from "../../../files/assets/shared/desktop/oracle-black.svg";
+import google from "../../../files/assets/shared/desktop/google-black.svg";
+import nvidia from "../../../files/assets/shared/desktop/nvidia-black.svg";
 
-const Partner = styled.img`
-  max-width: 120px;
-  height: 100px;
-  width: 100px;
-  mask: url(${(props) => props.logoName}) no-repeat 50% 50%;
-  mask-size: contain;
-  background-color: ${variables.colors.sec_blue};
+const Partner = styled.img.attrs((props) => ({
+  src: props.src,
+}))`
+  src: (${(props) => props.logoName}) no-repeat 50% 50%;
+  justify-self: center;
+  align-self: center;
+  margin-top: 40px;
+  z-index: 1;
+`;
+
+const PartnerContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+
+  @media (min-width: 600px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+
+  @media (min-width: 700px) {
+    margin: 0 115px;
+  }
+
+  @media (min-width: 1280px) {
+    margin: 0;
+    padding-top: 0;
+    column-gap: 65px;
+    row-gap: 55px;
+  }
+`;
+
+const PartnersTitle = styled.h3`
+  font-family: ${variables.fonts.titleFont};
+  font-size: 24px;
+  font-weight: 400;
+  color: #36536b;
+  text-align: center;
+  opacity: 75%;
+  margin-top: 80px; ;
 `;
 
 const ContactPartners = () => {
   return (
     <>
       <section>
-        <p>Join the thousands of innovators already building with us</p>
-        <div>
-          <Partner logoName={tesla}></Partner>
-          <Partner logoName={microsoft}></Partner>
-          <Partner logoName={hp}></Partner>
-          <Partner logoName={oracle}></Partner>
-          <Partner logoName={google}></Partner>
-          <Partner logoName={nvidia}></Partner>
-        </div>
+        <PartnersTitle>Join the thousands of innovators already building with us </PartnersTitle>
+        <PartnerContainer>
+          <Partner src={tesla}></Partner>
+          <Partner src={microsoft}></Partner>
+          <Partner src={hp}></Partner>
+          <Partner src={oracle}></Partner>
+          <Partner src={google}></Partner>
+          <Partner src={nvidia}></Partner>
+        </PartnerContainer>
       </section>
     </>
   );
