faa1c2d0f3d3fd0b7186edee35174f79ccc34613
BartoszZdunczyk <zdunczyk.bart@gmail.com>
2022-09-05 23:06:00 +0200

    Update Contact.jsx

--- src/components/UI/pages/contact/Contact.jsx
+++ src/components/UI/pages/contact/Contact.jsx
@@ -5,10 +5,30 @@ import ContactPartners from "../../organisms/contact/ContactPartners";
 import ContactForm from "../../organisms/ContactForm";
 import { variables } from "../../../../settings/mainstyles";
 
+const Main = styled.main`
+  @media (min-width: 1200px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const Section = styled.section`
   display: flex;
   flex-direction: column;
-  margin: 80px 24px;
+  margin: 0 24px 80px 24px;
+
+  @media (min-width: 600px) {
+    margin: 0 161px 80px 161px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 445px;
+    margin: 0 0 80px 50px;
+  }
+
+  @media (min-width: 1440px) {
+    margin: 0 0 80px 160px;
+  }
 `;
 
 const ContactTitle = styled.h3`
@@ -17,24 +37,34 @@ const ContactTitle = styled.h3`
   font-weight: 400;
   color: ${variables.colors.sec_blue};
   text-align: center;
-  margin: 0 0 40px 0;
+  margin: 80px 24px 40px 24px;
+
   @media (min-width: 600px) {
-    font-size: 48px;
+    font-size: 47px;
+    letter-spacing: -0.369231px;
+    line-height: 56px;
+    margin: 80px 161px 40px 161px;
   }
   @media (min-width: 1200px) {
     text-align: left;
-    margin: 0;
+    margin: 80px 0 40px 50px;
+    width: 600px;
+  }
+  @media (min-width: 1440px) {
+    margin-left: 160px;
   }
 `;
 
 const Contact = () => {
   return (
     <>
-      <Section>
-        <ContactTitle>Submit a help request and we’ll get in touch shortly.</ContactTitle>
-        <ContactForm />
+      <ContactTitle>Submit a help request and we’ll get in touch shortly.</ContactTitle>
+      <Main>
+        <Section>
+          <ContactForm />
+        </Section>
         <ContactPartners />
-      </Section>
+      </Main>
     </>
   );
 };
